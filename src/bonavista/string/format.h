4fc2387ca38dce8e6b89d5c33ec26366fae2ca02
Eric Dingle <eric.dingle@gmail.com>
2011-10-12 20:07:21 -0400

    Adding string lib.

---
+++ src/bonavista/string/format.h
@@ -0,0 +1,12 @@
+#ifndef BONAVISTA_STRING_FORMAT_H_
+#define BONAVISTA_STRING_FORMAT_H_
+
+#include <string>
+
+namespace string {
+
+std::string Format(const char* format, ...);
+
+}  // namespace string
+
+#endif
