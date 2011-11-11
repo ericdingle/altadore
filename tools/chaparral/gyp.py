1e874c9f1c2dc040a34a2d3d31bfcbf1c817fe90
Eric Dingle <eric.dingle@gmail.com>
2011-11-10 23:16:43 -0500

    Move tools files in chaparral subfolder.

---
+++ tools/chaparral/gyp.py
@@ -0,0 +1,2 @@
+from ..bonavista import gyp
+gyp.Run(__file__, '..', '../../src/chaparral/chaparral.gyp');
--- tools/gyp.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-sys.path.append('../..')
-
-from bonavista import gyp
-gyp.Run(__file__, '../..', '../chaparral.gyp');
