8f182991f7e51afef083df5fa2a7b17c4343402d
Eric Dingle <eric.dingle@gmail.com>
2011-11-11 16:41:59 -0500

    Split scoped_ptr into scoped_array.

--- src/bonavista/memory/scoped_ptr.h
+++ src/bonavista/memory/scoped_ptr.h
@@ -77,29 +77,6 @@ class scoped_ptr : public scoped_ptr_base<T, VoidFunc, DeletePtrFunc<T> > {
   DISALLOW_COPY_AND_ASSIGN(scoped_ptr);
 };
 
-template <typename T>
-struct DeleteArrayFunc {
-  void operator()(T* ptr) const {
-    delete [] ptr;
-  }
-};
-
-template <typename T>
-class scoped_array : public scoped_ptr_base<T, VoidFunc, DeleteArrayFunc<T> > {
- public:
-  explicit scoped_array(T* ptr=NULL) : scoped_ptr_base<T, VoidFunc, DeleteArrayFunc<T> >(ptr) {}
-  ~scoped_array() {}
-
-  T& operator[](uint index) {
-    T* ptr = scoped_ptr_base<T, VoidFunc, DeleteArrayFunc<T> >::ptr();
-    DASSERT(ptr != NULL);
-    return ptr[index];
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(scoped_array);
-};
-
 }  // namespace memory
 
 #endif
--- src/bonavista/memory/scoped_ptr_test.cpp
+++ src/bonavista/memory/scoped_ptr_test.cpp
@@ -80,22 +80,3 @@ TEST(ScopedPtrTest, ConstructorAndDestructor) {
   }
   EXPECT_EQ(testing::InstCount::inst_count(), 0);
 }
-
-TEST_CASE(ScopedArrayTest) {
-};
-
-TEST(ScopedArrayTest, ConstructorAndDestructor) {
-  EXPECT_EQ(testing::InstCount::inst_count(), 0);
-  {
-    memory::scoped_array<testing::InstCount> array(new testing::InstCount[5]);
-    EXPECT_EQ(testing::InstCount::inst_count(), 5);
-  }
-  EXPECT_EQ(testing::InstCount::inst_count(), 0);
-}
-
-TEST(ScopedArrayTest, IndexOperator) {
-  memory::scoped_array<int> array(new int[5]);
-  array[2] = 10;
-  EXPECT_EQ(array[2], 10);
-  EXPECT_EQ(array.ptr()[2], 10);
-}
