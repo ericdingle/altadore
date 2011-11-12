7798ef83d977211e83492e57fce818ddeb37791d
Eric Dingle <eric.dingle@gmail.com>
2011-11-12 08:59:38 -0800

    Add InstCountT testing class.

--- src/bonavista/testing/inst_count_test.cpp
+++ src/bonavista/testing/inst_count_test.cpp
@@ -2,14 +2,29 @@
 
 #include "bonavista/testing/test_case.h"
 
-TEST_CASE(InstanceCountTest) {
+TEST_CASE(InstCountTest) {
 };
 
-TEST(InstanceCountTest, ConstructorAndDestructor) {
+TEST(InstCountTest, ConstructorAndDestructor) {
   EXPECT_EQ(testing::InstCount::inst_count(), 0);
   {
     testing::InstCount count;
     EXPECT_EQ(testing::InstCount::inst_count(), 1);
   }
   EXPECT_EQ(testing::InstCount::inst_count(), 0);
+}
+
+class Dummy1 : public testing::InstCountT<Dummy1> {};
+class Dummy2 : public testing::InstCountT<Dummy2> {};
+
+TEST(InstCountTest, TemplateConstructorAndDestructor) {
+  EXPECT_EQ(Dummy1::inst_count(), 0);
+  EXPECT_EQ(Dummy2::inst_count(), 0);
+  {
+    Dummy1 dummy1;
+    EXPECT_EQ(Dummy1::inst_count(), 1);
+    EXPECT_EQ(Dummy2::inst_count(), 0);
+  }
+  EXPECT_EQ(Dummy1::inst_count(), 0);
+  EXPECT_EQ(Dummy2::inst_count(), 0);
 }
