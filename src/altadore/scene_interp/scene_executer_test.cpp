97aa74d61957d51adfbcb744f96b0f333c0607b9
Eric Dingle <eric.dingle@gmail.com>
2012-02-27 03:21:13 +1100

    Add Light to scene interp classes.

--- src/altadore/scene_interp/scene_executer_test.cpp
+++ src/altadore/scene_interp/scene_executer_test.cpp
@@ -4,6 +4,7 @@
 #include "altadore/scene_interp/scene_lexer.h"
 #include "altadore/scene_interp/scene_parser.h"
 #include "altadore/shader/color.h"
+#include "altadore/shader/light.h"
 #include "altadore/shader/material.h"
 #include "altadore/util/invokable.h"
 #include "bonavista/testing/test_case.h"
@@ -122,6 +123,11 @@ TEST(SceneExecuterTest, ExecuteNew) {
   memory::scoped_refptr<Invokable> object;
   EXPECT_TRUE(var_->Get(object.Receive()));
   EXPECT_NOT_NULL(dynamic_cast<Color*>(object.ptr()));
+
+  Init("new Light(new Point3(1.0, 2.0, 3.0), new Color(0.1, 0.2, 0.3));");
+  EXPECT_TRUE(executer_->Execute(var_.Receive()));
+  EXPECT_TRUE(var_->Get(object.Receive()));
+  EXPECT_NOT_NULL(dynamic_cast<Light*>(object.ptr()));
 
   Init("new Material(new Color(0.1, 0.2, 0.3), 25.0, 1.0);");
   EXPECT_TRUE(executer_->Execute(var_.Receive()));
